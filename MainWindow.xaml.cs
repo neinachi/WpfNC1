ecf66d402b5f3dd22193ee74101877b3ba90c194
Grigory Romanov <g.o.romanov@gmail.com>
2019-11-01 13:46:17 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace WpfNC1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        
+              MySqlConnection con = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=bimhdb; password=Romanoleg1");
+              con.Open();
+              MySqlDataAdapter adp = new MySqlDataAdapter("select * from bimhdb.goods", con);
+              DataTable dt = new DataTable();
+              adp.Fill(dt);
+              DataGrid1.DataContext = dt;
+            
+        }
+
+
+    }
+}
+
